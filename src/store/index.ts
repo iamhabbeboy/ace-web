c31af2f5bede5e01158521a47ebd7749389c00c8
Abiodun Azeez <iamhabbeboy@gmail.com>
2023-07-18 08:27:50 +0100

    Disable logger for dev environment

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,23 +1,27 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import logger from 'redux-logger'
-import rootReducer from "./collections"
-import storageSession from 'redux-persist/lib/storage/session' 
-import { persistStore, persistReducer } from 'redux-persist'
+import logger from "redux-logger";
+import rootReducer from "./collections";
+import storageSession from "redux-persist/lib/storage/session";
+import { persistStore, persistReducer } from "redux-persist";
 // import { checkUserAccountType } from '../middleware/checkUserAccountType';
- 
+
 const persistConfig = {
-  key: 'root',
+  key: "root",
   version: 1,
   storage: storageSession,
   // blacklist: [""],
-}
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
   reducer: persistedReducer,
-  middleware: getDefaultMiddleware => getDefaultMiddleware({ serializableCheck: false, }).concat(logger),
-  devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    process.env.NODE_ENV === "development"
+      ? getDefaultMiddleware({ serializableCheck: false }).concat(logger)
+      : getDefaultMiddleware({ serializableCheck: false }),
+  devTools: process.env.NODE_ENV === "development",
 });
-const persistor = persistStore(store)
+const persistor = persistStore(store);
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
@@ -26,4 +30,4 @@ export type AppThunk<ReturnType = void> = ThunkAction<
   unknown,
   Action<string>
 >;
-export {store, persistor}
+export { store, persistor };
